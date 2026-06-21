3c24b9faa248eeed12f7c31321e8c12d482e1528
Ali <120646614+alizamanian79@users.noreply.github.com>
2026-06-21 14:23:06 +0330

    payment requet handled

--- src/main/java/com/app/server/controller/TransactionController.java
+++ src/main/java/com/app/server/controller/TransactionController.java
@@ -2,6 +2,7 @@ package com.app.server.controller;
 
 import com.app.server.util.wallet_service_producer.WalletRMQProducer;
 import com.app.server.util.wallet_service_producer.dto.request.PaymentRequestDto;
+import com.app.server.util.wallet_service_producer.dto.response.WalletResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -9,16 +10,17 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/transaction")
+@RequestMapping("/api/v1/payment")
 public class TransactionController {
 
     private final WalletRMQProducer walletRMQProducer;
     
 
-//    @PostMapping("/payment")
-//    public ResponseEntity<?> list(Authentication auth){
-//        return ResponseEntity.status().body();
-//    }
+    @PostMapping("/request")
+    public ResponseEntity<?> paymentRequest(@RequestBody PaymentRequestDto req){
+        WalletResponseDto res = walletRMQProducer.paymentRequest(req);
+        return ResponseEntity.status(res.getStatus()).body(res);
+    }
 
 
 }
